c748ca30ae15eeb3f4c525aef9337a0d1c89d5c5
Prajwal <153988038+Prajwal-Poojary@users.noreply.github.com>
2025-06-09 16:15:03 +0530

    Update report.component.ts

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import { Document, Packer, Paragraph, TextRun, HeadingLevel, Table, TableRow, TableCell, WidthType, ImageRun } from 'docx';
-import { saveAs } from 'file-saver';
+// import { saveAs } from 'file-saver';
 import { Chart, registerables } from 'chart.js';
 
 @Component({
