3057b21438503962ce5cb388d6f1671f1f98a307
Prajwal <153988038+Prajwal-Poojary@users.noreply.github.com>
2025-06-29 23:27:43 +0530

    last

--- backend/run.py
+++ backend/run.py
@@ -3,5 +3,8 @@ import os
 
 app = create_app()
 
+def is_debug():
+    return os.environ.get('FLASK_DEBUG', '0') == '1'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=is_debug())
