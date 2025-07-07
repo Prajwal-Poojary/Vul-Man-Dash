1445a9eff01d6aab7fe8a1492682bbf53d6778aa
Prajwal <153988038+Prajwal-Poojary@users.noreply.github.com>
2025-07-07 18:37:46 +0530

    nothing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://final-backend1-td6h.onrender.com/api',
   apiUrl2: 'https://final-backend2-2vvt.onrender.com/api2',
-  apiUrlFlask: 'https://final-backend-hpcs.onrender.com/api'
+  apiUrlFlask: 'https://final-backend-hpcs.onrender.com/api',
+  frontendUrl: 'https://vulnarability-management-dashboard.onrender.com'
 }; 
