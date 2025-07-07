733ed9548c599a6f2eba6eec7b1a87bb844f0834
Prajwal <153988038+Prajwal-Poojary@users.noreply.github.com>
2025-07-07 21:54:22 +0530

    new

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     CORS(app, origins=["https://vulnarability-management-dashboard.onrender.com"])
+    CORS(app)
     
     from .routes import report_routes
     app.register_blueprint(report_routes)
